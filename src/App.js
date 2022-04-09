c96bd3852fbb152b709e9f08f7cf5754685bd0d1
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-04-09 13:36:15 -0400

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // Import
 import React from 'react'
 import './App.css'
+import Header from './Header.jsx'
 import Menu from './Menu';
 
 function App() {
   return (
     <div>
+      <Header />
       <Menu />
     </div>
   );
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+// Import
+import './Header.css'
+
+// Function
+function Header(props) {
+  // Return
+  return (
+    <div>
+      <h1>Wanka Inc.</h1>
+      <h2>Financial informations</h2>
+    </div>
+  )
+}
+
+// Export 
+export default Header
